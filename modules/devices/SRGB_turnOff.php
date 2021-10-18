4b9f589fa2eb38afbf5ec5a1e1b7c74a6a6b96f5
tarasfrompir <tarasfrompir@inbox.ru>
2021-10-18 11:23:33 +0300

    Delete SRGB_turnOff.php

--- modules/devices/SRGB_turnOff.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$color = $this->getProperty('color');
-if ($color != '000000') $this->setProperty('colorSaved', $this->getProperty('color'));
-$this->setProperty('color', '000000');
-$this->setProperty('status', 0);
