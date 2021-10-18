43d3d046c3107cc17b3d01b3f65473eb4bb4ae68
tarasfrompir <tarasfrompir@inbox.ru>
2021-10-18 11:25:33 +0300

    Delete SThermostats_switchEnable.php

--- modules/devices/SThermostats_switchEnable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ($this->getProperty('disabled')) {
-  $this->setProperty('disabled', 0);
-} else {
-  $this->setProperty('disabled', 1);
-  $this->setProperty('relay_status', 0); // turn off
-}
