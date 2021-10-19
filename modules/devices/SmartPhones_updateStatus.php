5c6921aabbfeb91b5774da5e5025ddcd72a8fefb
tarasfrompir <tarasfrompir@inbox.ru>
2021-10-19 22:22:18 +0300

    Update SmartPhones_updateStatus.php

--- modules/devices/SmartPhones_updateStatus.php
+++ modules/devices/SmartPhones_updateStatus.php
@@ -1,9 +1,8 @@
 <?php
 $new_value =json_decode($params['NEW_VALUE']);
 $old_value =json_decode($params['OLD_VALUE']);
-if (strval($new_value['latitude']) == strval($old_value['latitude']) and strval($new_value['longitude']) == strval($old_value['longitude'])) return;
+if (strval($new_value['latitude']) == strval($old_value['latitude']) and strval($new_value['longitude']) == strval($old_value['longitude']) and strval($new_value['charging']) == strval($old_value['charging'])) return;
 $out = json_decode($new_value, true);
 $this->setProperty('coordinates', trim($out['latitude']) . ',' . trim($out['longitude']) );
 $this->setProperty('speed', trim($out['speed']) );
 $this->setProperty('battery', trim($out['battlevel']));
-
